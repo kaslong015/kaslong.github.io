c80f026c83f7d2467c04eadb9f87a42492ca213f
kaslong <kevinkaslong@gmail.com>
2021-08-23 18:27:26 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import './App.css';
-import Navbar from './navbar'
-import Home from './home'
+import Navbar from './Navbar'
+import Home from './Home'
 function App() {
   // const title = 'welcome to my react class';
   
