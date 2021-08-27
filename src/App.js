b12ff9a7476f166cb831ed8b2fa76021674c8f2b
kaslong <kevinkaslong@gmail.com>
2021-08-27 10:14:23 +0100

    react Link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 // import './App.css';
 import Navbar from './Navbar'
 import Home from './Home'
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+import Create from './Create';
+
 function App() {
   // const title = 'welcome to my react class';
   
   return (
-    <div className="App">  
-    <Navbar/>  
-     <div className="content">
-       <Home />
-     </div>
-    </div>
+    <Router>
+      <div className="App">  
+      <Navbar/>  
+      <div className="content">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>  
+          <Router path="/create">
+            <Create />
+          </Router>        
+        </Switch>        
+      </div>
+      </div>
+    </Router>
+    
   );
 }
 
